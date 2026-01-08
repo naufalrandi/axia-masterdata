b687cc0061be0933f0c0f456a3e055bd23a5db50
Muhammad Mu'min Azis <muhammadmuminaziz@gmail.com>
2026-01-08 15:38:44 +0700

    refactor ConsultancyMethod model for improved readability and structure

--- src/models/consultancymethod.js
+++ src/models/consultancymethod.js
@@ -1,29 +1,30 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class ConsultancyMethod extends Model {
     static associate(models) {
       // define association here
     }
   }
-  ConsultancyMethod.init({
-    name: DataTypes.STRING,
-    description: DataTypes.TEXT,
-    durations: {
-      type: DataTypes.JSONB,
-      get() {
-        const rawValue = this.getDataValue('durations');
-        return rawValue ? JSON.parse(rawValue) : null;
+  ConsultancyMethod.init(
+    {
+      name: DataTypes.STRING,
+      description: DataTypes.TEXT,
+      durations: {
+        type: DataTypes.JSONB,
+        // get() {
+        //   const rawValue = this.getDataValue('durations');
+        //   return rawValue ? JSON.parse(rawValue) : null;
+        // },
+        set(value) {
+          this.setDataValue("durations", JSON.stringify(value));
+        },
       },
-      set(value) {
-        this.setDataValue('durations', JSON.stringify(value));
-      }
+    },
+    {
+      sequelize,
+      modelName: "ConsultancyMethod",
     }
-  }, {
-    sequelize,
-    modelName: 'ConsultancyMethod',
-  });
+  );
   return ConsultancyMethod;
 };
